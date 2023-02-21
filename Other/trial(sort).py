5b65a17aa3477de04bfbcde242b4d8d2514d329c
D4NGK4 <danjosheps.djt98@gmail.com>
2023-02-21 18:39:54 +0800

    bubble sort fixed

--- Other/trial(sort).py
+++ Other/trial(sort).py
@@ -2,10 +2,13 @@ lst = [1, 3, 5, 2, 4]
 
 
 def sorting(array):
-    for i in range(len(array)):
-        for j in range(len(array)+1):
-            if array[j] < array[i]:
-                array[j] = array[i]
+    for i in range(0, (len(array)-1)):
+        for j in range(len(array)-1):
+            if array[j] > array[j+1]:
+                temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
+    return array
 
 
 print(sorting(lst))
